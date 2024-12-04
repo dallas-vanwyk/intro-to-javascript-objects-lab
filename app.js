b3a8118591c465bcd5809789ffbb3c2216742c0a
dallas-vanwyk <dallas.vanwyk@gmail.com>
2024-12-03 18:06:51 -0600

    initial commit, completed through exercise 4

---
+++ app.js
@@ -0,0 +1,60 @@
+const pokemon = require('./data.js');
+
+const game = {
+    party: [],
+    gyms: [
+        { location: "Pewter City", completed: false, difficulty: 1 },
+        { location: "Cerulean City", completed: false, difficulty: 2 },
+        { location: "Vermilion City", completed: false, difficulty: 3 },
+        { location: "Celadon City", completed: false, difficulty: 4 },
+        { location: "Fuchsia City", completed: false, difficulty: 5 },
+        { location: "Saffron City", completed: false, difficulty: 6 },
+        { location: "Cinnabar Island", completed: false, difficulty: 7 },
+        { location: "Viridian City", completed: false, difficulty: 8 },
+    ],
+    items: [
+        { name: "potion", quantity: 4 },
+        { name: "pokeball", quantity: 8 },
+        { name: "rare candy", quantity: 99 },
+    ],
+}
+
+// display every item in the array
+// console.dir(pokemon, { maxArrayLength: null });
+
+// log JUST the name of the pokemon number 59
+// console.log(pokemon[58].name);
+
+// familiarize with game
+// console.log(game);
+
+
+/*
+Exercise 3
+1. Add a new property to the `game` object. Let's call it "difficulty".
+2. Choose a value for "difficulty" that you think fits the game. Ex: "Easy", "Med" or "Hard". How would you assign it?
+
+
+Solve Exercise 3 here:
+*/
+
+
+game.difficulty = "easy";
+// console.log(game);
+
+
+/*
+Exercise 4
+1. Select a starter Pokémon from the `pokemon` array. Remember, a starter Pokémon's `starter` property is true.
+2. Add this Pokémon to the `game.party` array. Which array method will you use to add them?
+
+
+Solve Exercise 4 here:
+*/
+
+console.log(game.party);
+game.party.push(pokemon[6]);
+console.log(game.party);
+
+
+
