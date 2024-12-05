8c131454ef919c0733011608b2d0d643edbb7b31
dallas-vanwyk <dallas.vanwyk@gmail.com>
2024-12-04 20:25:27 -0600

    completed through exercise 11 although several exercises need follow-up

--- app.js
+++ app.js
@@ -70,6 +70,7 @@ Solve Exercise 5 here:
 */
 
 // I DID NOT REALIZE THIS WAS ASKING ME TO ADD ONLY STARTER POKEMON?? per exercise 7. Thaaaaaaat makes more sense but is definitely not clear here
+// ok but exercise 9 makes it seem like maybe this detail wasn't required here? Idk I'm leaving it
 
 // initial quick-and-dirty of just picking random pokemon because _student did not understand the assignment_
 // game.party.push(pokemon[40]);
@@ -80,14 +81,14 @@ Solve Exercise 5 here:
 
 // console.log(pokemon.length);
 
-for (let i=0; i<pokemon.length; i++) {
-    if(pokemon[i].starter) {
-        console.log(pokemon[i].number);
+for (let i = 0; i < pokemon.length; i++) {
+    if (pokemon[i].starter && pokemon[i].number !== game.party[0].number) {
+        // console.log(pokemon[i].number);
         game.party.push(pokemon[i]);
     }
 }
-// need to make this exclude the pikachu that we added in exercise 
-
+// need to make this exclude the pikachu that we added in exercise 4
+// ok I quick-and-dirty did that. currently it's not checking the entire party but w/e
 
 
 
@@ -129,9 +130,105 @@ More Hints: The existing starter Pokemon will be *replaced* in your party with t
 Solve Exercise 7 here:
 */
 
-// console.log(game.party)
+// console.log(game.party);
 
 // again a quick and dirty for loop÷
 for (let i = 0; i < game.party.length; i++) {
-    console.log(game.party[i].number);
+    let evolvedNumber = game.party[i].number;
+    game.party[i] = pokemon[evolvedNumber];
 }
+// console.log(game.party);
+
+// why would I need the SPLICE method for this??
+
+
+
+
+/*
+Exercise 8
+1. Print the name of each Pokémon in your party.
+2. Consider using a loop or an array method to access each Pokémon's name.
+
+Solve Exercise 8 here:
+*/
+
+// very quick, very dirty
+console.log("current party:")
+for (let i = 0; i < game.party.length; i++) {
+    console.log(game.party[i].name);
+}
+console.log(""); // line break
+
+
+/*
+Exercise 9
+1. Can you print out all the starter Pokémon from the `pokemon` array?
+2. Think about how you can identify a starter Pokémon and then log their names.
+
+
+Solve Exercise 9 here:
+*/
+
+console.log("starter pokemon:");
+for (let i = 0; i < pokemon.length; i++) {
+    if (pokemon[i].starter) {
+        console.log(pokemon[i].name);
+    }
+}
+
+
+/*
+Exercise 10
+Create a method called `catchPokemon` and add it to the `game` object. You should not need to edit the original game object directly. This method should:
+  - Accept an object as a parameter called `pokemonObj`
+  - Add the `pokemonObj` to the `game.party` array.
+  - not return anything
+
+After writing this method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+
+Solve Exercise 10 here:
+*/
+
+
+
+const catchPokemon = (pokemonObj) => {
+    game.party.push(pokemonObj);
+};
+
+// game.push(catchPokemon)
+
+catchPokemon(pokemon[77]);
+console.log("after latest catch:");
+// console.log(game.party);
+
+// ok I need to brush up on adding this as a method to the game array BUT for now... this isn't right but it is close
+//                    F O L L O W      U P
+
+
+
+/*
+Exercise 11
+1. Copy the `catchPokemon` method that you just wrote above, and paste it below. Modify it so that it also decreases the number of pokeballs in your inventory each time you catch a Pokémon.
+2. How will you find and update the quantity of pokeballs in the `game.items` array?
+
+Tips:
+For this exercise, it's okay to have a negative number of pokeballs.
+After updating the method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+Also, log the `game.items` array to confirm that the pokeball quantity is being decremented.
+
+Solve Exercise 11 here:
+*/
+
+
+// console.log(game.items[1]);
+const catchPokemonWithBall = (pokemonObj) => {
+    game.party.push(pokemonObj);
+    // need to select game items by the NAME rather than the index but again
+    // quick and dirty completion is better than incompletion when you're already behind the deadline
+    game.items[1].quantity--;
+};
+
+catchPokemonWithBall(pokemon[42]);
+console.log(game.items);
+
+
